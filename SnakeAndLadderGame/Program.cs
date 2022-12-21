8679edf906ed2a417d9fad89489e78eaef63098f
Surajk37 <surajkhot9993@gmail.com>
2022-12-21 16:16:09 +0530

    [SURAJ] Welcome Message added

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome To Snake And Ladder Program\n");
+            Console.ReadLine();
+        }
+    }
+}
