e301065bc743161ba2e01f62eccff1833ee13b24
Surajk37 <surajkhot9993@gmail.com>
2022-12-23 21:27:32 +0530

    [Suraj] Players checks for options

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -4,7 +4,7 @@ namespace SnakeAndLadderGame
 {
     class Program
     {
-       const int position = 0;
+      public const int position = 0;
         public void StartPlayingPosition()
         {
             //UC1: If Dice Value Is Zero Than You Can Start Game Otherwise Not Using Random
@@ -31,11 +31,45 @@ namespace SnakeAndLadderGame
             Console.WriteLine("Dice Value is :" + a);
             return ;
         }
+        public void CheckOption(int position)
+        {
+            // UC3-To Check for Option like Noplay,Snakke,Laddder
+            Console.WriteLine("The postition of player starts From : {0} ", position);
+            const int NoPlay = 0;
+            const int Ladder = 1;
+            const int Snake = 2;
+            Random r = new Random();
+            int dice = r.Next(1, 7);//Random Number Between 1 to 6 Including 6
+            int opt = r.Next(1, 3);//Random Number Between 1 to 2 Including 2
+            switch (opt)
+            {
+                case NoPlay:
+                    //If Random Number Is 0
+                    Console.WriteLine("Stay at Same Place :" + position);
+                    break;
+
+                case Ladder:
+                    //If Random Number Is 1
+                    position += dice;
+                    Console.WriteLine("You Move Ahead:" + position + "\t"+"Dice Value is {0}", dice);
+                    break;
+
+                case Snake:
+                    //If Random Number Is 2
+                    position -= dice;
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+                    Console.WriteLine("You move Behind:" + position + "\t" + "Dice Value is {0}", dice);
+                    break;
+            }
+        }
         public static void Main(string[] args)
         {
             Console.WriteLine("\t\tWelcome To Snake And Ladder Program\n");
             Console.WriteLine("Select a option from list");
-            Console.WriteLine("1.UC-1 \n2.UC-2");
+            Console.WriteLine("1.UC-1 \n2.UC-2 \n3.UC-3");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -47,6 +81,11 @@ namespace SnakeAndLadderGame
                     case 2:
                     Console.WriteLine("\tUC2-Player rolls a die to get a number between 1 to 6");
                     Program.RollsDie();
+                    break;
+                case 3:
+                    Console.WriteLine("\tUC3-Player checks for a options");
+                    Program program = new Program();
+                    program.CheckOption(position);
                     break;
                 default:
                     Console.WriteLine("Enter a valid option");
